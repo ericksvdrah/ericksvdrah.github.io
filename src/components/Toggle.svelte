86080c3bf923ab817f7d764ad0b19f27161cffe1
Erick Saavedra <esvdrah@gmail.com>
2024-02-15 21:56:25 -0500

    feat: user can be change mode dark and light

--- src/components/Toggle.svelte
+++ src/components/Toggle.svelte
@@ -1,17 +1,33 @@
 <script>
-  import Night from './icons/Night.svelte';
+  import { onMount } from 'svelte';
   import Light from './icons/Light.svelte';
+  import Night from './icons/Night.svelte';
 
-  export let darkMode = false;
+  let isDarkMode = false;
+
+  const updateTheme = (darkMode) => {
+    document.documentElement.classList.toggle('dark', darkMode);
+    localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+  };
+
+  const toggleTheme = () => {
+    isDarkMode = !isDarkMode;
+    updateTheme(isDarkMode);
+  };
+
+  onMount(() => {
+    isDarkMode = localStorage.getItem('theme') === 'dark';
+    updateTheme(isDarkMode);
+  });
 </script>
 
 <button
-  on:click={() => (darkMode = !darkMode)}
+  on:click={toggleTheme}
   class="h-12 w-12 rounded-lg p-2 hover:bg-gray-100 dark:hover:bg-gray-700"
 >
-  {#if darkMode}
-    <Night />
-  {:else}
+  {#if isDarkMode}
     <Light />
+  {:else}
+    <Night />
   {/if}
 </button>
